3f68c4118e7ab931610ab7051c428086992b12b0
varunteja542 <parshavenivarunteja@gmail.com>
2024-10-15 10:30:53 +0530

    Delete public directory

--- public/script.js
+++
@@ -1,7 +0,0 @@
-let menubar = document.querySelector('#menu-bars');
-let navbar  = document.querySelector('.navbar');
-
-menubar.onclick = () =>{
-    menubar.classList.toggle('fa-times');
-    navbar.classList.toggle('active')
-}
