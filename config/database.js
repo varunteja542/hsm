e1c9290e9f519d22430ee7872c25541cdbf352bc
varunteja542 <parshavenivarunteja@gmail.com>
2024-10-19 21:26:49 +0530

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+// Create a connection to the MySQL database
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',  // Replace with your database username
+  password: '12345vt6',  // Replace with your database password
+  database: 'hospitalmanagement',  // Your database name
+  charset: 'utf8mb4' // Use utf8mb4 instead of 'cesu8'
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the MySQL database:', err);
+    throw err;
+  }
+  console.log('Connected to the MySQL database');
+});
+
+module.exports = db;
