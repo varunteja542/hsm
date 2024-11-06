21be16afb78a086533acdb5b053a5384f990a615
varunteja542 <parshavenivarunteja@gmail.com>
2024-11-06 20:51:17 +0530

    Update startserver.js

--- startserver.js
+++ startserver.js
@@ -1,5 +1,5 @@
 const app = require('./server');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
